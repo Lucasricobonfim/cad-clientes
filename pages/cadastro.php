5d265524489e482e56a10b96629da7d3ee7654a5
Lucas Bonfim <aprendiz113@gazin.net.br>
2023-01-05 11:53:17 -0300

    f

--- pages/cadastro.php
+++ pages/cadastro.php
@@ -3,12 +3,12 @@
 include('header.php'); ?>
 <div class="position-absolute top-50 start-50 translate-middle formCad">
     <h2 class="text-center">Cadastro e clientes</h2>
-    <form action="" class="">
+    <form action="" class="" method="post">
         <input type="text" name="nome" placeholder="nome">
         <input type="text" name="cpf_cnpj" placeholder="cpf_cnpj">
         <input type="text" name="idade" placeholder="idade">
         <input type="date" name="nascimento" placeholder="nascimento">
-        <input type="text" class="btn btn-warning" value="CADASTRAR">
+        <input type="submit" class="btn btn-warning" >
     </form>
 </div>
 <?php include("footer.php"); ?>
